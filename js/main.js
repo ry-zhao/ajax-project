1e7defdd1657fcb2097306c2cbe92f6484049ddb
ry-zhao <ryanzhao21@gmail.com>
2021-09-22 12:46:27 -0700

    Add event listener to image URL input that fires upon user input and updates image element's src attribute to correspond with input value. #1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var $recipeTitle = document.querySelector('#recipe-title');
 var $recipeTitleInput = document.querySelector('#title');
+var $recipePhoto = document.querySelector('#recipe-photo');
+var $recipePhotoUrlInput = document.querySelector('#photo-url');
 
 var recipe = {
   title: '',
@@ -11,5 +13,11 @@ var recipe = {
 function updateRecipeTitle(event) {
   $recipeTitle.textContent = event.target.value;
 }
+
+function updateRecipePhoto(event) {
+  $recipePhoto.setAttribute('src', event.target.value);
+}
+
+$recipePhotoUrlInput.addEventListener('input', updateRecipePhoto);
 
 $recipeTitleInput.addEventListener('input', updateRecipeTitle);
