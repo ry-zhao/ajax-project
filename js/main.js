ee0e8f551c52d5c5c5c305d195314304e32f8f63
ry-zhao <ryanzhao21@gmail.com>
2021-09-22 12:59:35 -0700

    Add event listener to add ingredient button that fires upon click and updates the list to include the value of the add ingredient input. #1

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ var $recipeTitle = document.querySelector('#recipe-title');
 var $recipeTitleInput = document.querySelector('#title');
 var $recipePhoto = document.querySelector('#recipe-photo');
 var $recipePhotoUrlInput = document.querySelector('#photo-url');
+var $ingredientList = document.querySelector('#ingredient-list');
+var $addIngredientInput = document.querySelector('#add-ingredient');
+var $addIngredientButton = document.querySelector('#add-ingredient-button');
 
 var recipe = {
   title: '',
@@ -17,6 +20,16 @@ function updateRecipeTitle(event) {
 function updateRecipePhoto(event) {
   $recipePhoto.setAttribute('src', event.target.value);
 }
+
+function addIngredient(event) {
+  event.preventDefault();
+  var newIngredient = document.createElement('li');
+  newIngredient.textContent = $addIngredientInput.value;
+  $addIngredientInput.value = '';
+  $ingredientList.append(newIngredient);
+}
+
+$addIngredientButton.addEventListener('click', addIngredient);
 
 $recipePhotoUrlInput.addEventListener('input', updateRecipePhoto);
 
