e2e4852de0628eb991b0e460fda8b80627ae3446
ry-zhao <ryanzhao21@gmail.com>
2021-09-22 12:40:42 -0700

    Add event listener to title input that fires upon user input an updates title element to correspond with input value. #1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
+var $recipeTitle = document.querySelector('#recipe-title');
+var $recipeTitleInput = document.querySelector('#title');
+
 var recipe = {
   title: '',
   photoUrl: '',
   ingredients: [],
   instructions: []
 };
+
+function updateRecipeTitle(event) {
+  $recipeTitle.textContent = event.target.value;
+}
+
+$recipeTitleInput.addEventListener('input', updateRecipeTitle);
